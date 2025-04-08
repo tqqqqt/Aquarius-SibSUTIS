6cb6ecea7755d81b64cb809f2f460d2590a8d868
Tq Tq <tqqqqt@users.noreply.github.com>
2025-04-08 16:14:19 +0700

    fix errors to correct work

--- lab4/openbmc_main.py
+++ lab4/openbmc_main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.service import Service as ChromeService
 import time
+import tempfile
 
 #--------------------------------
 # defines
@@ -18,12 +19,16 @@ def startWork(_adress):
 		driver.quit()
 
 	chromedriver_path="/bin/chromedriver"
+	user_data_dir=tempfile.mkdtemp()
 
 	options=webdriver.ChromeOptions()
-	options.add_argument('headless')
+	options.add_argument('--headless')
 	options.add_argument('--ignore-certificate-errors')
 	options.add_argument('--ignore-ssl-errors')
 	options.add_argument('--disable-blink-features=AutomationControlled')
+	options.add_argument('--disable-dev-shm-usage')
+	options.add_argument('--no-sandbox')
+	options.add_argument(f"--user-data-dir={user_data_dir}")
 	service=ChromeService(executable_path=chromedriver_path)
 
 	driver=webdriver.Chrome(service=service,options=options)
